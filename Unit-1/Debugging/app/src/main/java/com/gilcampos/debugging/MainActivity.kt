1324b1ab881c210f99384d505e51a35fb43a3f97
gil <47683202+Gil-Campos@users.noreply.github.com>
2022-12-09 13:08:19 -0600

    Added a function to render the result of a division in the ui without issues using threads

--- Unit-1/Debugging/app/src/main/java/com/gilcampos/debugging/MainActivity.kt
+++ Unit-1/Debugging/app/src/main/java/com/gilcampos/debugging/MainActivity.kt
@@ -3,6 +3,8 @@ package com.gilcampos.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import kotlin.concurrent.thread
 
 private const val TAG = "MainActivity"
 
@@ -13,6 +15,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         logging()
+        division()
+    }
+
+    private fun division() {
+        val numerator = 60
+        var denominator = 4
+
+        /**
+         * new thread that can run the repeat loop. Otherwise, Thread.sleep(3000) would block the
+         * main thread, and the app view would not render until onCreate() is finished (including
+         * division() with its repeat loop).
+         */
+        thread(start = true) {
+            repeat(4) {
+                Thread.sleep(3000)
+
+                /**
+                 * If you try running the app now, you notice a FATAL EXCEPTION. The reason for this
+                 * exception is that only threads that created a view are allowed to change it.
+                 * Luckily, you can reference the UI thread using runOnUiThread().
+                 */
+                runOnUiThread {
+                    findViewById<TextView>(R.id.division_textview).setText("${numerator / denominator}")
+                    denominator--
+                }
+            }
+        }
     }
 
     private fun logging() {
