70668045e96c29a988eead447709d4f6973f663b
gil <47683202+Gil-Campos@users.noreply.github.com>
2022-12-10 16:59:38 -0600

    Now the app has viewbinding enable and uses good coding practices it needs a better UI and UX but it gets the work done just fine for now

--- Unit-2/Tip-Time/app/src/main/java/com/gilcampos/tiptime/MainActivity.kt
+++ Unit-2/Tip-Time/app/src/main/java/com/gilcampos/tiptime/MainActivity.kt
@@ -2,10 +2,51 @@ package com.gilcampos.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gilcampos.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * The lateinit keyword is something new. It's a promise that your code will initialize the
+     * variable before using it. If you don't, your app will crash.
+     */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayTip(0.0)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
