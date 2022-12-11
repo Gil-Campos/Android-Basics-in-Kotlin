82e50ba411c4eed39a0cc9d17d353985ec20bfb3
gil <47683202+Gil-Campos@users.noreply.github.com>
2022-12-10 22:55:50 -0600

    Made some changes to enhace the user experience like adding icons,  hiding the keyboard when the user is hits the enter key in the soft keyboard

--- Unit-2/Tip-Time/app/src/main/java/com/gilcampos/tiptime/MainActivity.kt
+++ Unit-2/Tip-Time/app/src/main/java/com/gilcampos/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.gilcampos.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.gilcampos.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -20,6 +24,12 @@ class MainActivity : AppCompatActivity() {
 
         displayTip(0.0)
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun calculateTip() {
@@ -48,5 +58,16 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide Keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
